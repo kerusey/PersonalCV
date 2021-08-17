dbc8beefb7430e7f31a9f8f7362a91a112450a25
kerusey <keruseyoff@gmail.com>
2021-08-17 04:16:15 +0300

    getting prepared for the production

--- PersonalCV/urls.py
+++ PersonalCV/urls.py
@@ -4,8 +4,13 @@ from django.contrib import admin
 from . import views
 from django.conf.urls.static import static
 from django.urls import path
+from django.views.static import serve
 
 urlpatterns = [
     url('admin/', admin.site.urls),
     path('', views.index, name='Home'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += [
+   url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT, 'show_indexes': settings.DEBUG})
+]
