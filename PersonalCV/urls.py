2e5fc8ec943e0ccec6855db6e4047771748a302c
kerusey <keruseyoff@gmail.com>
2021-08-22 17:32:45 +0300

    removed admin panel url support (no actual need)

--- PersonalCV/urls.py
+++ PersonalCV/urls.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from django.conf.urls import url, handler404, handler500
-from django.contrib import admin
 from . import views
 from django.conf.urls.static import static
 from django.urls import path
@@ -9,7 +8,6 @@ from django.views.static import serve
 app_name = "PersonalCV"
 
 urlpatterns = [
-    url('admin/', admin.site.urls),
     path('', views.index, name='Home'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
